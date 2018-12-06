7db58beb8515966a6e1daed4d9b20e0b2b629a4a
arianenld <42517443+arianenld@users.noreply.github.com>
2018-12-06 21:29:14 +0800

    Update actions.js

--- store/actions.js
+++ store/actions.js
@@ -1,7 +1,26 @@
+import axios from 'axios'
 export default {
-  showPokemon ({ commit }, pokeData) {
-    commit('showPokemonMutation', pokeData)
+  showPokemon ({ commit }, pokeKey) {
+    this.dispatch('showLoader', true)
+    axios.get(https://pokeapi.co/api/v2/pokemon/${pokeKey}/)
+         .then(response => {
+           if(response.status === 200){
+             this.dispatch('showLoader', false)
+             let data = response.data
+             let pokeData = {
+                id: data.id,
+                name: data.name,
+                sprite: data.sprites.front_default,
+                type2: data.types[1] == undefined ? "none" : data.types[1].type.name,
+                type: data.types[0] == undefined ? "none" : data.types[0].type.name
+             }
+             commit('showPokemonMutation', pokeData)
+           }
+         }).catch(err => {
+            this.dispatch('showLoader', false)
+         })
   },
+  
   showLoader ({ commit }, toggle) {
     commit('showLoaderMutation', toggle)
   }
